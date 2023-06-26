32a2aa024139e67b034a7a9ca33e4513d6747c91
GustavoTZ14 <gterrin155@gmail.com>
2023-06-26 19:52:35 -0300

    commit

--- init.lua
+++ init.lua
@@ -18,8 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  --colorscheme = "astrodark",
-  colorscheme = "kanagawa",
+  colorscheme = "astrodark",
+  --colorscheme = "catppuccin",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -80,7 +80,8 @@ return {
     -- define the separators between each section
     separators = {
       left = { "", " " }, -- separator for the left side of the statusline
-      right = { " ", "" }, -- separator for the right side of the statusline
+      --right = { " ", "" }, -- separator for the right side of the statusline
+      right = { " ", "" },
       tab = { "", "" },
     },
     -- add new colors that can be used by heirline
@@ -109,6 +110,35 @@ return {
     },
   },
 
+  highlights = {
+    -- set highlights for all themes
+    -- use a function override to let us use lua to retrieve colors from highlight group
+    -- there is no default table so we don't need to put a parameter for this function
+    init = function()
+      local get_hlgroup = require("astronvim.utils").get_hlgroup
+      -- get highlights from highlight groups
+      local normal = get_hlgroup "Normal"
+      local fg, bg = normal.fg, normal.bg
+      local bg_alt = get_hlgroup("Visual").bg
+      local green = get_hlgroup("String").fg
+      local red = get_hlgroup("Error").fg
+      -- return a table of highlights for telescope based on colors gotten from highlight groups
+      return {
+        TelescopeBorder = { fg = bg_alt, bg = bg },
+        TelescopeNormal = { bg = bg },
+        TelescopePreviewBorder = { fg = bg, bg = bg },
+        TelescopePreviewNormal = { bg = bg },
+        TelescopePreviewTitle = { fg = bg, bg = green },
+        TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
+        TelescopePromptNormal = { fg = fg, bg = bg_alt },
+        TelescopePromptPrefix = { fg = red, bg = bg_alt },
+        TelescopePromptTitle = { fg = bg, bg = red },
+        TelescopeResultsBorder = { fg = bg, bg = bg },
+        TelescopeResultsNormal = { bg = bg },
+        TelescopeResultsTitle = { fg = bg, bg = bg },
+      }
+    end,
+  },
 
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
