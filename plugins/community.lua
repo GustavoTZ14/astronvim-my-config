50f939bea917f6767c407208fbece48b57181e70
GustavoTZ14 <gterrin155@gmail.com>
2023-06-28 17:42:46 -0300

    commit

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,26 +4,5 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.nightfox-nvim", enabled = false },
-  { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
-  { import = "astrocommunity.colorscheme.rose-pine" },
-  { -- further customize the options set by the community
-    "catppuccin",
-    opts = {
-      integrations = {
-        sandwich = false,
-        noice = true,
-        mini = true,
-        leap = true,
-        markdown = true,
-        neotest = true,
-        cmp = true,
-        overseer = true,
-        lsp_trouble = true,
-        ts_rainbow2 = true,
-      },
-    },
-  },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
